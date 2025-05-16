bed8f9fa0aaad1b211b4ac9739216741b5a5c0ca
smd995 <smd99540@gmail.com>
2025-05-16 15:38:32 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-200 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
